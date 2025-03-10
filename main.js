431eb548402d8129f48707bb5f5ebe16deb277d7
Damey <damej7711@gmail.com>
2025-03-10 19:04:16 +0300

    formatcode

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const nav1=document.querySelector("#nav")
-const img=document.querySelector("#image")
-const burger1=document.querySelector("#burger")
-burger1.addEventListener('click',()=>{
-    if(nav1.classList.toggle("open"))
-        img.src="close.svg"
-    else{
-        img.src="burger.svg"
-    }
-})
+const nav1 = document.querySelector("#nav");
+const img = document.querySelector("#image");
+const burger1 = document.querySelector("#burger");
+burger1.addEventListener("click", () => {
+  if (nav1.classList.toggle("open")) img.src = "close.svg";
+  else {
+    img.src = "burger.svg";
+  }
+});
